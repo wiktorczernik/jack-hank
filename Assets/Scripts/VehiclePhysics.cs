5b2da12db6caef9662f897cf04ed6c5714806c6a
Viktor Chernikov <vktrchernikov@gmail.com>
2025-03-13 15:02:42 +0100

    chore: Renamed all VehiclePhysics properties, just so they make more sense

--- Assets/Scripts/VehiclePhysics.cs
+++ Assets/Scripts/VehiclePhysics.cs
@@ -5,63 +5,110 @@ using UnityEngine.InputSystem;
 [RequireComponent(typeof(Rigidbody))]
 public class VehiclePhysics : MonoBehaviour
 {
-    public int speedKmh => Mathf.RoundToInt(BodyRigidbody.linearVelocity.magnitude * 3.6f);
-    public int speedKmhForward => Mathf.Abs(Mathf.RoundToInt(Vector3.Dot(BodyRigidbody.linearVelocity, transform.forward) * 3.6f));
-
-    public VehicleSkidmarkSource skidmarkSourceLeft;
-    public VehicleSkidmarkSource skidmarkSourceRight;
-
-    public ParticleSystem smokeSourceLeft;
-    public ParticleSystem smokeSourceRight;
-
-    public Rigidbody BodyRigidbody => bodyRigidbody;
-    public Transform CenterOfMass;
-
-    [Header("Wheels")]
-    public CarWheel[] wheels;
-
-    [Header("Settings")]
-    public float MaxSpeed = 100;
-    public float MaxBackwardSpeed = 15f;
-    public float Acceleration = 2300f; 
-    public float BrakeForce = 4500f;
-    public float TurnForce = 5000f;
-    public float CounterTurnForce = 7500;
-    public float TurnStabilizeSpeed = 8f;
-    public float DriftTurnForce = 3000f;
-    public float DriftMoveForce = 100f;
-    public float DriftAngle = 45f;
-    public float DriftSpeeddown = 3;
-    public float DriftSpeedup = 5;
-    public float DriftMinSpeed = 15f;
-    public float MinFrictionMagnitude = 50f;
-    public float MaxFrictionForce = 50f;
-    public float DriftCounterSpeedup = 8;
-    public float MaxDriftAngularY = 1.5f;
-    public float DriftStartThreshold = 1.1f;
-    public float DriftEndThreshold = 0.4f;
-    public float DriftEndMaxTimer = 1.0f;
-    public float AdditionalWheelGravity = 50f;
-    public AnimationCurve TestCurve;
-    public AnimationCurve TurnForceCurve;
-
-    [Header("Input")]
-    [SerializeField] Vector2 moveInput = Vector2.zero;
+    #region State
     [Header("State")]
-    public bool isDrifting = false;
-    public bool isDriftingRight = false;
-    public float driftAngularY = 0.0f;
-    public float driftEndTimer = 0.0f;
+    [SerializeField] bool _isDrifting = false;
+    [SerializeField] bool _isDriftingRight = false;
+    [SerializeField] Vector2 _input = Vector2.zero;
+    [SerializeField] float _driftAngular = 0.0f;
+    [SerializeField] float _driftEndBuildup = 0.0f;
+    public bool isDrifting { get => _isDrifting; private set => _isDrifting = value; }
+    public bool isDriftingRight { get => _isDriftingRight; private set => _isDriftingRight = value; }
+    public Vector3 input { get => _input; private set => _input = value; }
+    public float driftAngular { get => _driftAngular; private set => _driftAngular = value; }
+    public float driftEndBuildup { get => _driftEndBuildup; private set => _driftEndBuildup = value; }
+    public int speedKmh => Mathf.RoundToInt(bodyRigidbody.linearVelocity.magnitude * 3.6f);
+    public int speedKmhForward => Mathf.Abs(Mathf.RoundToInt(Vector3.Dot(bodyRigidbody.linearVelocity, transform.forward) * 3.6f));
+    #endregion
+
+    #region Speed
+    [Header("Speed")]
+    [SerializeField] float _maxForwardSpeed = 100f;
+    [SerializeField] float _maxBackwardSpeed = 100f;
+    [SerializeField] float _forwardAcceleration = 100f;
+    [SerializeField] float _forwardBrakeForce = 100f;
+    public float maxForwardSpeed { get => _maxForwardSpeed; private set => _maxForwardSpeed = value; }
+    public float maxBackwardSpeed { get => _maxBackwardSpeed; private set => _maxBackwardSpeed = value; }
+    public float forwardAcceleration { get => _forwardAcceleration; private set => _forwardAcceleration = value; }
+    public float forwardBrakeForce { get => _forwardBrakeForce; private set => _forwardBrakeForce = value; }
+    #endregion
+
+    #region Turning
+    [Header("Turning")]
+    [SerializeField] float _turnForce = 5000f;
+    [SerializeField] float _turnAginForce = 7500;
+    [SerializeField] float _turnStabilization = 8f;
+    [SerializeField] AnimationCurve _turnForceCurve;
+    public float turnForce { get => _turnForce; private set => _turnForce = value; }
+    public float turnAginForce { get => _turnAginForce; private set => _turnAginForce = value; }
+    public float turnStabilization { get => _turnStabilization; private set => _turnStabilization = value; }
+    public AnimationCurve turnForceCurve { get => _turnForceCurve; private set => _turnForceCurve = value; }
+    #endregion
+
+    #region Wheels
+    [Header("Wheels")]
+    [SerializeField] CarWheel[] _wheels;
+    [SerializeField] float _bonusWheelGravity = 50f;
+    public CarWheel[] wheels { get => _wheels; private set => _wheels = value; }
+    public float bonusWheelGravity { get => _bonusWheelGravity; private set => _bonusWheelGravity = value; }
+    #endregion
+
+    #region Drifting
+    [Header("Drifting")]
+    [SerializeField] float _driftMinSpeed = 15f;
+    [SerializeField] float _driftTurnForce = 3000f;
+    [SerializeField] float _driftMoveForce = 1500f;
+    [SerializeField] float _driftLeanAngle = 15f;
+    [SerializeField] float _driftClimbRate = 1.7f;
+    [SerializeField] float _driftClimbAginRate = 3.75f;
+    [SerializeField] float _driftSinkRate = 6.25f;
+    [SerializeField] float _driftMaxAngular = 1.5f;
+    [SerializeField] float _driftStartAngular = 1.1f;
+    [SerializeField] float _driftEndAngular = 0.5f;
+    [SerializeField] float _driftEndTime = 0.5f;
+    public float driftMinSpeed { get => _driftMinSpeed; private set => _driftMinSpeed = value; }
+    public float driftTurnForce { get => _driftTurnForce; private set => _driftTurnForce = value; }
+    public float driftMoveForce { get => _driftMoveForce; private set => _driftMoveForce = value; }
+    public float driftLeanAngle { get => _driftLeanAngle; private set => _driftLeanAngle = value; }
+    public float driftClimbRate { get => _driftClimbRate; private set => _driftClimbRate = value; }
+    public float driftClimbAginRate { get => _driftClimbAginRate; private set => _driftClimbAginRate = value; }
+    public float driftSinkRate { get => _driftSinkRate; private set => _driftSinkRate = value; }
+    public float driftMaxAngular { get => _driftMaxAngular; private set => _driftMaxAngular = value; }
+    public float driftStartAngular { get => _driftStartAngular; private set => _driftStartAngular = value; }
+    public float driftEndAngular { get => _driftEndAngular; private set => _driftEndAngular = value; }
+    public float driftEndTime { get => _driftEndTime; private set => _driftEndTime = value; }
+    #endregion
+
+    #region Physics
+    [Header("Physics")]
+    [SerializeField] Rigidbody _bodyRigidbody;
+    [SerializeField] Transform _centerOfMass;
+    [SerializeField] float _frictionForce = 750f;
+    public Rigidbody bodyRigidbody { get => _bodyRigidbody; private set => _bodyRigidbody = value; }
+    public Transform centerOfMass { get => _centerOfMass; private set => _centerOfMass = value; }
+    public float frictionForce { get => _frictionForce; private set => _frictionForce = value; }
+    #endregion
+
+    #region Visuals
+    [Header("Visuals")]
+    [Obsolete]
+    public VehicleSkidmarkSource skidmarkSourceLeft;
+    [Obsolete]
+    public VehicleSkidmarkSource skidmarkSourceRight;
+    [Obsolete]
+    public ParticleSystem smokeSourceLeft;
+    [Obsolete]
+    public ParticleSystem smokeSourceRight;
+    #endregion
 
 
     public void OnMove(InputValue value)
     {
-        Vector2 input = value.Get<Vector2>();
-        moveInput = input;
+        input = value.Get<Vector2>();
     }
     public float GetForwardSpeed()
     {
-        return Vector3.Dot(BodyRigidbody.linearVelocity, transform.forward);
+        return Vector3.Dot(bodyRigidbody.linearVelocity, transform.forward);
     }
     public void Accelerate(float force = 1.0f)
     {
@@ -69,11 +116,11 @@ public class VehiclePhysics : MonoBehaviour
         Quaternion driftRotation = Quaternion.identity;
         if (isDrifting)
         {
-            float driftFactor = driftAngularY / MaxDriftAngularY;
-            driftRotation = Quaternion.Euler(0, driftFactor * DriftAngle, 0);
+            float driftFactor = driftAngular / driftMaxAngular;
+            driftRotation = Quaternion.Euler(0, driftFactor * driftLeanAngle, 0);
         }
-        float sp = Vector3.Dot(BodyRigidbody.linearVelocity, direction) * 3.6f;
-        if (sp > MaxSpeed)
+        float sp = Vector3.Dot(bodyRigidbody.linearVelocity, direction) * 3.6f;
+        if (sp > maxForwardSpeed)
         {
             Debug.Log("Capped front");
             return;
@@ -87,35 +134,35 @@ public class VehiclePhysics : MonoBehaviour
             }
         }
         force = Mathf.Clamp01(force) * wheelGroundFactor;
-        BodyRigidbody.AddForceAtPosition(driftRotation * direction * Acceleration * force, CenterOfMass.position);
+        bodyRigidbody.AddForceAtPosition(driftRotation * direction * forwardAcceleration * force, centerOfMass.position);
     }
 
     public void Brake(float force = 1.0f)
     {
-        if (Vector3.Dot(BodyRigidbody.linearVelocity, -transform.forward) * 3.6f > MaxBackwardSpeed)
+        if (Vector3.Dot(bodyRigidbody.linearVelocity, -transform.forward) * 3.6f > maxBackwardSpeed)
         {
             Debug.Log("Capped back");
             return;
         }
         force = Mathf.Clamp01(force);
-        BodyRigidbody.AddForceAtPosition(-transform.forward * BrakeForce * force, CenterOfMass.position);
+        bodyRigidbody.AddForceAtPosition(-transform.forward * forwardBrakeForce * force, centerOfMass.position);
     }
     public void DoTurn(float input)
     {
         if (input == 0) return;
         input = Mathf.Clamp(input, -1, 1);
-        if (isDrifting) return;
-        float sp = Mathf.Abs(Vector3.Dot(BodyRigidbody.linearVelocity, transform.forward));
+        if (_isDrifting) return;
+        float sp = Mathf.Abs(Vector3.Dot(bodyRigidbody.linearVelocity, transform.forward));
         if (GetForwardSpeed() < 0)
         {
             input = -input;
         }
         sp *= 3.6f;
-        sp /= MaxSpeed;
+        sp /= maxForwardSpeed;
         sp = Mathf.Clamp01(sp);
-        sp = TurnForceCurve.Evaluate(sp);
+        sp = turnForceCurve.Evaluate(sp);
         bool isCounter = Mathf.Sign(bodyRigidbody.angularVelocity.y) != Mathf.Sign(input);
-        BodyRigidbody.AddTorque(transform.up * input * (isCounter ? CounterTurnForce : TurnForce) * sp);
+        bodyRigidbody.AddTorque(transform.up * input * (isCounter ? turnAginForce : turnForce) * sp);
     }
     public void DoDrift(float input)
     {
@@ -124,20 +171,20 @@ public class VehiclePhysics : MonoBehaviour
         if (input != 0)
         {
             float driftSign = isDriftingRight ? 1 : -1;
-            float chosenFactor = DriftSpeedup;
-            if (Mathf.Sign(input) != Mathf.Sign(driftAngularY))
+            float chosenFactor = driftClimbRate;
+            if (Mathf.Sign(input) != Mathf.Sign(driftAngular))
             {
-                chosenFactor = DriftCounterSpeedup;
+                chosenFactor = driftClimbAginRate;
             }
-            driftAngularY += input * chosenFactor * Time.fixedDeltaTime;
+            driftAngular += input * chosenFactor * Time.fixedDeltaTime;
         }
         else
         {
-            driftAngularY = Mathf.SmoothStep(driftAngularY, 0, DriftSpeeddown * Time.fixedDeltaTime);
+            driftAngular = Mathf.SmoothStep(driftAngular, 0, driftSinkRate * Time.fixedDeltaTime);
         }
-        driftAngularY = Mathf.Clamp(driftAngularY, -MaxDriftAngularY, MaxDriftAngularY);
+        driftAngular = Mathf.Clamp(driftAngular, -driftMaxAngular, driftMaxAngular);
         Vector3 newAngular = bodyRigidbody.angularVelocity;
-        newAngular.y = driftAngularY;
+        newAngular.y = driftAngular;
         bodyRigidbody.angularVelocity = newAngular;
     }
     void HandleWheels()
@@ -146,7 +193,7 @@ public class VehiclePhysics : MonoBehaviour
         {
             wheel.isDrifting = isDrifting;
             wheel.ApplyFriction();
-            wheel.ApplyGravity(AdditionalWheelGravity);
+            wheel.ApplyGravity(bonusWheelGravity);
         }
     }
     void HandleSkidmarks()
@@ -156,8 +203,8 @@ public class VehiclePhysics : MonoBehaviour
         {
             if (isDrifting)
             {
-                float factor = Mathf.Abs(bodyRigidbody.angularVelocity.y) - DriftEndThreshold;
-                if (factor > 0.1f && driftEndTimer <= float.Epsilon)
+                float factor = Mathf.Abs(bodyRigidbody.angularVelocity.y) - driftEndAngular;
+                if (factor > 0.1f && driftEndBuildup <= float.Epsilon)
                 {
                     smokeSourceRight.Play();
                 }
@@ -178,7 +225,7 @@ public class VehiclePhysics : MonoBehaviour
         {
             if (isDrifting)
             {
-                float factor = Mathf.Abs(bodyRigidbody.angularVelocity.y) - DriftEndThreshold;
+                float factor = Mathf.Abs(bodyRigidbody.angularVelocity.y) - driftEndAngular;
                 if (factor > 0.1f)
                 {
                     smokeSourceLeft.Play();
@@ -205,30 +252,30 @@ public class VehiclePhysics : MonoBehaviour
 
         if (isDrifting)
         {
-            if (absAngY < DriftEndThreshold || speedKmh < DriftMinSpeed)
+            if (absAngY < driftEndAngular || speedKmh < driftMinSpeed)
             {
-                driftEndTimer += Time.fixedDeltaTime;
-                if (driftEndTimer >= DriftEndMaxTimer)
+                driftEndBuildup += Time.fixedDeltaTime;
+                if (driftEndBuildup >= driftEndTime)
                 {
                     isDrifting = false;
                     isDriftingRight = false;
-                    driftAngularY = 0.0f;
-                    driftEndTimer = 0.0f;
+                    driftAngular = 0.0f;
+                    driftEndBuildup = 0.0f;
                 }
             }
             else
             {
-                driftEndTimer = 0.0f;
+                driftEndBuildup = 0.0f;
             }
         }
         else
         {
-            if (absAngY > DriftStartThreshold && speedKmh > DriftMinSpeed)
+            if (absAngY > driftStartAngular && speedKmh > driftMinSpeed)
             {
                 isDrifting = true;
                 isDriftingRight = angY > 0;
-                driftAngularY = angY;
-                driftEndTimer = 0.0f;
+                driftAngular = angY;
+                driftEndBuildup = 0.0f;
             }
         }
     }
@@ -236,12 +283,12 @@ public class VehiclePhysics : MonoBehaviour
     {
         Vector3 velocity = bodyRigidbody.linearVelocity;
         float magnitude = velocity.magnitude;
-        Vector3 frictionForce = -velocity.normalized;
-        if (magnitude >= MinFrictionMagnitude)
+        if (magnitude > float.Epsilon)
         {
-            frictionForce *= MaxFrictionForce;
+            Vector3 frictionForce = -velocity.normalized;
+            frictionForce *= this.frictionForce;
+            bodyRigidbody.AddForce(frictionForce);
         }
-        bodyRigidbody.AddForce(frictionForce);
     }
 
     private void FixedUpdate()
@@ -253,34 +300,44 @@ public class VehiclePhysics : MonoBehaviour
         HandleWheels();
         HandleSkidmarks();
 
-        if (moveInput.y > 0 && isPlaying)
+        if (input.y > 0 && isPlaying)
         {
-            Accelerate(moveInput.y);
+            Accelerate(input.y);
         }
-        if (moveInput.y < 0 && isPlaying)
+        if (input.y < 0 && isPlaying)
         {
-            Brake(-moveInput.y);
+            Brake(-input.y);
         }
         if (isPlaying)
         {
-            DoTurn(moveInput.x);
-            DoDrift(moveInput.x);
+            DoTurn(input.x);
+            DoDrift(input.x);
         }
         ApplyFriction();
-        if (!isDrifting && moveInput.x == 0)
+        if (!isDrifting && input.x == 0)
         {
             Vector3 av = bodyRigidbody.angularVelocity;
             av.y = 0;
-            bodyRigidbody.angularVelocity = Vector3.Lerp(bodyRigidbody.angularVelocity, av, TurnStabilizeSpeed * Time.fixedDeltaTime);
+            bodyRigidbody.angularVelocity = Vector3.Lerp(bodyRigidbody.angularVelocity, av, turnStabilization * Time.fixedDeltaTime);
         }
     }
 
-    void Awake()
+#if UNITY_EDITOR
+    private void OnValidate()
     {
-        bodyRigidbody = GetComponent<Rigidbody>();
-        bodyRigidbody.maxLinearVelocity = 150f;
+        if (bodyRigidbody == null)
+        {
+            bodyRigidbody = GetComponent<Rigidbody>();
+            bodyRigidbody.maxLinearVelocity = maxForwardSpeed;
+        }
+        if (centerOfMass == null)
+        {
+            centerOfMass = bodyRigidbody.transform;
+        }
+        if (wheels.Length == 0)
+        {
+            wheels = GetComponentsInChildren<CarWheel>();
+        }
     }
-
-
-    Rigidbody bodyRigidbody;
+#endif
 }
