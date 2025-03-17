ea2cae0dfc7ee5469bdb4db1f55f4ad1a071c95d
Dan <danik.serduk@gmail.com>
2025-03-17 11:47:36 +0100

    feat: safe bounty points marked with type of bounty bonus; replace handling of collision events with passengers and smashableEntity

--- Assets/Scripts/GameRunInfo.cs
+++ Assets/Scripts/GameRunInfo.cs
@@ -1,36 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class GameRunInfo
 {
-    public float time = 0;
-    public int bountyPoints = 0;
-    public int passengersOnBoard = 0;
+    public float Time = 0;
+    public int AllBountyPoints => _pointsByBonusTypes.Sum(pair => pair.Value) ;
+    public int PassengersOnBoard = 0;
     
-    const string _formatTimeTemplate = "{0}:{1}.{2}";
-    
-    
+    private const string FormatTimeTemplate = "{0}:{1}.{2}";
+    private readonly Dictionary<PlayerBonusTypes, int> _pointsByBonusTypes = new()
+    {
+        { PlayerBonusTypes.DestructionCombo , 0},
+        { PlayerBonusTypes.Drift , 0},
+        { PlayerBonusTypes.Flying, 0},
+        { PlayerBonusTypes.Passenger, 0},
+    };
+
+
     public string GetTimeFormatted()
     {
-        float seconds = time % 60;
-        int minutes = (int)((time - seconds) / 60);
-        float millisec = seconds % 1;
+        var seconds = Time % 60;
+        var minutes = (int)((Time - seconds) / 60);
+        var millisecond = seconds % 1;
 
-        string secondsPadded = Mathf.RoundToInt(seconds).ToString().PadLeft(2, '0');
-        string millisecPadded = millisec.ToString().Substring(2, 2).PadLeft(2, '0');
+        var secondsPadded = Mathf.RoundToInt(seconds).ToString().PadLeft(2, '0');
+        var millisecondPadded = millisecond.ToString().Substring(2, 2).PadLeft(2, '0');
         
-        return string.Format(_formatTimeTemplate, minutes, secondsPadded, millisecPadded);
+        return string.Format(FormatTimeTemplate, minutes, secondsPadded, millisecondPadded);
     }
-    public void AddBountyPoints(int amount)
+
+    public void ChangeBonusBountyBy(int value, PlayerBonusTypes bonusType)
     {
-        bountyPoints += amount;
+        if (!_pointsByBonusTypes.ContainsKey(bonusType))
+            throw new NotImplementedException(
+                "Bonus type has not implemented in GameRunInfo!!! Please add a BonusType!");
+        
+        if (bonusType == PlayerBonusTypes.Passenger) AddPassenger();
+        
+        _pointsByBonusTypes[bonusType] += value;
     }
-    public void AddBountyPenalty(int penalty)
-    {
-        bountyPoints -= penalty;
-    }
+    
     public void AddPassenger()
     {
-        passengersOnBoard++;
+        PassengersOnBoard++;
     }
 }
 
