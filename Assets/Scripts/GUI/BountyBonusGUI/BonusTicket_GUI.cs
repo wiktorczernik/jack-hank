699d235b56a886d30e95331dddbe93b46ab89a0c
Dan <danik.serduk@gmail.com>
2025-03-20 09:46:43 +0100

    feat: add UI for small bonuses; rework BonusDetector and develop it to BonusSystem; add settings presets to bonus system

---
+++ Assets/Scripts/GUI/BountyBonusGUI/BonusTicket_GUI.cs
@@ -0,0 +1,52 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class BonusTicket_GUI : MonoBehaviour
+{
+    [SerializeField] private string bonusName;
+    [SerializeField] private PlayerBonusTypes bonusType;
+    [SerializeField] private float timeToShowWithoutUpdatesInSeconds;
+    
+    public PlayerBonusTypes BonusType => bonusType;
+    
+    private float _lastUpdateInSeconds;
+    private TMP_Text _text;
+
+    public void ChangeBonusValueOn(int value)
+    {
+        _lastUpdateInSeconds = Time.time;
+        UpdateView(value);
+    }
+    
+    public void ChangeBonusValueOn(int value, int comboValue)
+    {
+        if (bonusType != PlayerBonusTypes.DestructionCombo) 
+            throw new System.Exception("This overload of ChangeBonusValueOn should be used only for PlayerBonusTypes.DestructionCombo!");
+        
+        _lastUpdateInSeconds = Time.time;
+        UpdateView(value);
+    }
+
+    private void Start()
+    {
+        _text = GetComponent<TMP_Text>();
+        gameObject.SetActive(false);
+        enabled = false;
+    }
+
+    private void Update()
+    {
+        if (!(Time.time - _lastUpdateInSeconds > timeToShowWithoutUpdatesInSeconds)) return;
+        
+        gameObject.SetActive(false);
+        enabled = false;
+    }
+
+    private void UpdateView(int value)
+    {
+        gameObject.SetActive(true);
+        enabled = true;
+        
+        _text.text = $"{bonusName.ToUpper()}: {value}";
+    }
+}
