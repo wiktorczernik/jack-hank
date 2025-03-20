6df18a375c492ae370be38ab6bdaa778b19549ec
Viktor Chernikov <vktrchernikov@gmail.com>
2025-03-21 00:40:53 +0100

    feat: Camera controller, invokable shake

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,56 @@
+using Cinemachine;
+using System.Collections;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public CinemachineVirtualCamera virtualCamera;
+    public CinemachineBasicMultiChannelPerlin perlinNoise;
+
+    [Header("State")]
+    public float shakeIntensity = 0f;
+
+    [Header("Settings")]
+    public float shakeMaxAmplitude = 50f;
+    public float shakeRiseRate = 4f;
+    public float shakeFallRate = 0.5f;
+
+    public void Shake(float intensity)
+    {
+        intensity = Mathf.Clamp01(intensity);
+
+        if (intensity > shakeIntensity)
+        {
+            StopAllCoroutines();
+            StartCoroutine(ShakeRise(intensity));
+        }
+    }
+
+    IEnumerator ShakeRise(float targetIntensity)
+    {
+        while (shakeIntensity < targetIntensity - float.Epsilon)
+        {
+            shakeIntensity = Mathf.Clamp01(shakeIntensity + shakeRiseRate * Time.deltaTime);
+            perlinNoise.m_AmplitudeGain = shakeMaxAmplitude * shakeIntensity;
+            yield return new WaitForEndOfFrame();
+        }
+        yield return ShakeFall();
+    }
+    IEnumerator ShakeFall()
+    {
+        while (shakeIntensity > 0)
+        {
+            shakeIntensity = Mathf.Clamp01(shakeIntensity - shakeFallRate * Time.deltaTime);
+            perlinNoise.m_AmplitudeGain = shakeMaxAmplitude * shakeIntensity;
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+#if UNITY_EDITOR
+    private void OnValidate()
+    {
+        virtualCamera = GetComponent<CinemachineVirtualCamera>();
+        perlinNoise = virtualCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+    }
+#endif
+}
