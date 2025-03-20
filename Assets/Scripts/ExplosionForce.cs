b820153ce81c713ecb97597000dc568600fd0d3a
Wiktor Czernik <git@czernik.dev>
2025-03-21 00:40:53 +0100

    feat: Camera controller, invokable shake

---
+++ Assets/Scripts/ExplosionForce.cs
@@ -0,0 +1,8 @@
+public static class ExplosionForce
+{
+    public const float none = 0.0f;
+    public const float weak = 0.25f;
+    public const float average = 0.5f;
+    public const float strong = 0.75f;
+    public const float epic = 1.0f;
+}
