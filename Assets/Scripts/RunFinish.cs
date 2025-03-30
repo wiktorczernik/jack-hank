c1d2cb0abc5ee01f20264c11044fa52efb3669b1
Dan <danik.serduk@gmail.com>
2025-03-30 17:23:13 +0200

    feat: show rank at the end of a level and open scene with levels

--- Assets/Scripts/RunFinish.cs
+++ Assets/Scripts/RunFinish.cs
@@ -1,12 +1,21 @@
+using System;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class RunFinish : MonoBehaviour
 {
+    [SerializeField] private FinishText_GUI finishText;
+
+    private void Start()
+    {
+        finishText.OnTextEndAnimation += () => SceneManager.LoadScene("XMenu");
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (!other.gameObject.CompareTag("Vehicle")) return;
             
         GameManager.FinishRun();
-        Debug.Log("Player finished");
+        finishText.ShowFinishMark(GameManager.GetMarkByBounty());
     }
 }
