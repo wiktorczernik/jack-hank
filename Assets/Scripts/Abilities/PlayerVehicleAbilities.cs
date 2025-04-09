1db8d2577356fa76fbdee2c9b173c838496ce43c
Viktor Chernikov <vktrchernikov@gmail.com>
2025-04-09 21:15:35 +0200

    feat: Ability requires mana to use it

--- Assets/Scripts/Abilities/PlayerVehicleAbilities.cs
+++ Assets/Scripts/Abilities/PlayerVehicleAbilities.cs
@@ -1,9 +1,17 @@
-using System;
 using UnityEngine;
 
 [RequireComponent(typeof(PlayerVehicle))]
 public class PlayerVehicleAbilities : MonoBehaviour
 {
+    [Header("Mana points")]
+    public int availableMana
+    {
+        get => _availableMana;
+        set => _availableMana = Mathf.Clamp(value, 0, _maxMana);
+    }
+    public int maxMana => _maxMana;
+
+    [Header("Abilities")]
     public PlayerNitroAbility nitro;
     public PlayerJumpAbility jump;
 
@@ -19,8 +27,21 @@ public class PlayerVehicleAbilities : MonoBehaviour
     public void OnNitro() => TryUseAbility(nitro);
     public void OnJump() => TryUseAbility(jump);
 
-    private void TryUseAbility(PlayerVehicleAbility ability)
+    private bool TryUseAbility(PlayerVehicleAbility ability)
     {
-        ability?.Use();
+        if (!ability) return false;
+        if (ability.requiredMana > availableMana) return false;
+        
+        if (ability.Use())
+        {
+            availableMana -= ability.requiredMana;
+            return true;
+        }
+
+        return false;
     }
+
+    [Header("Settings")]
+    [SerializeField] int _availableMana = 0;
+    [SerializeField] int _maxMana = 500;
 }
