c86b0b63d844f1f1243e78fcca50834e1581c786
Wiktor Czernik <git@czernik.dev>
2025-04-10 23:16:31 +0200

    chore: ExplosionProperties comments

--- Assets/Scripts/Explosion/ExplosionProperties.cs
+++ Assets/Scripts/Explosion/ExplosionProperties.cs
@@ -23,15 +23,36 @@ public struct ExplosionProperties
         this.shakeMaxDistance = shakeMaxDistance;
     }
 
+    /// <summary>
+    /// Transform of epicenter. Will override epicenterPosition if set
+    /// </summary>
     [SerializeField] Transform _epicenterParentTransform;
+    /// <summary>
+    /// World position of epicenter
+    /// </summary>
     [SerializeField] Vector3 _epicenterPosition;
 
+    /// <summary>
+    /// World position of epicenter
+    /// </summary>
     public Vector3 epicenterPosition
     {
         get => _epicenterParentTransform == null ? _epicenterPosition : _epicenterParentTransform.position;
     }
+    /// <summary>
+    /// Explosion force that objects within epicenter will feel
+    /// </summary>
     public float force;
+    /// <summary>
+    /// Radius of explosion epicenter (zone of destruction)
+    /// </summary>
     public float epicenterRadius;
+    /// <summary>
+    /// Intensity of camera shake
+    /// </summary>
     public float shakeIntensity;
+    /// <summary>
+    /// Maximum distance where player camera can still feel explosion shake
+    /// </summary>
     public float shakeMaxDistance;
 }
