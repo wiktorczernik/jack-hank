fa2f39a4f12e271c62e10cb00a86eb4f5a6bb64e
Viktor Chernikov <vktrchernikov@gmail.com>
2025-04-11 00:14:10 +0200

    feat: Cinematic player and cinematic sequence (partly migrated from Para/box)

---
+++ Assets/Scripts/Cinematics/CinematicSequence.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Cinematic Sequence", menuName = "Jack&Hank/Cinematic Sequence", order = 1)]
+public class CinematicSequence : ScriptableObject
+{
+    public float duration = -1f;
+    public GameObject prefab;
+    public AudioClip audio;
+
+    /// <summary>
+    /// Describes camera state at specific frame
+    /// </summary>
+    public struct CameraFrameState
+    {
+        /// <summary>
+        /// World position of camera in scene
+        /// </summary>
+        public Vector3 worldPosition;
+        /// <summary>
+        /// Rotation of camera in scene
+        /// </summary>
+        public Quaternion rotation;
+        /// <summary>
+        /// Field of view of camera
+        /// </summary>
+        public float fieldOfView;
+    }
+}
