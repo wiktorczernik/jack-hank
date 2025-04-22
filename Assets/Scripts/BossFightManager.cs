7230c86e61d52593c3802e9d894d468234d0561a
Dan <danik.serduk@gmail.com>
2025-04-22 19:40:47 +0200

    fit: add cutscenes for boss fight

--- Assets/Scripts/BossFightManager.cs
+++ Assets/Scripts/BossFightManager.cs
@@ -4,25 +4,63 @@ using UnityEngine;
 
 public abstract class BossFightManager : MonoBehaviour
 {
-    public event Action OnBegin;
-    public bool DuringFight { get; private set; }
-    public bool IsTriggered { get; private set; }
-    
-    [SerializeField] protected GameObject bossPrefab;
+    [SerializeField] protected Boss bossPrefab;
     [SerializeField] protected Transform bossSpawnPoint;
-    [SerializeField] private TriggerEventEmitter bossFightTrigger;
+    [SerializeField] private TriggerEventEmitter beginTrigger;
+    [SerializeField] private CinematicSequence beginCutscene;
+    [SerializeField] private Transform beginCutsceneTransform;
+    [SerializeField] private CinematicSequence endCutscene;
+    [SerializeField] private Transform endCutsceneTransform;
+
+    public bool duringFight { get; private set; }
+    public bool wasTriggered { get; private set; }
 
     private void Awake()
     {
-        if (!bossFightTrigger) 
-            Debug.LogError($"[{GetType().Name}] component in [{name}] game object: No trigger for boss fight!");
-        if (!bossPrefab) 
-            Debug.LogError($"[{GetType().Name}] component in [{name}] game object: No boss prefab!");
+        if (!beginTrigger)
+            Debug.LogError($"component [{GetType().Name}]; game object [{name}]: No trigger for boss fight!");
+        if (!bossPrefab)
+            Debug.LogError($"component [{GetType().Name}]; game object [{name}]: No boss prefab!");
         if (!bossSpawnPoint)
-            Debug.LogError($"[{GetType().Name}] component in [{name}] game object: No boss spawn point!");
-        
-        bossFightTrigger.OnEnter.AddListener(OnTriggerEnter);
+            Debug.LogError($"component [{GetType().Name}]; game object [{name}]: No boss spawn point!");
+        if (!beginCutscene)
+            Debug.LogError($"component [{GetType().Name}]; game object [{name}]: No begin cutscene!");
+        if (!beginCutsceneTransform)
+            Debug.LogError($"component [{GetType().Name}]; game object [{name}]: No begin cutscene transform!");
+        if (!endCutscene)
+            Debug.LogError($"component [{GetType().Name}]; game object [{name}]: No end cutscene!");
+        if (!endCutsceneTransform)
+            Debug.LogError($"component [{GetType().Name}]; game object [{name}]: No end cutscene transform!");
+
+        beginTrigger.OnEnter.AddListener(OnTriggerEnter);
+        bossPrefab.onDeath += OnBossDeath;
     }
+
+    private void OnTriggerEnter(Collider triggerCollider)
+    {
+        if (wasTriggered) return;
+
+        wasTriggered = true;
+        HandleTriggerEnter();
+    }
+
+    private void OnBossDeath()
+    {
+        CinematicPlayer.PlaySequence(endCutscene, endCutsceneTransform.position, endCutsceneTransform.rotation,
+            endCutsceneTransform.localScale);
+
+        CinematicPlayer.onEndPlay += OnCutsceneEnd;
+        return;
+
+        void OnCutsceneEnd()
+        {
+            HandleBossDeath();
+            duringFight = true;
+            CinematicPlayer.onEndPlay -= OnCutsceneEnd;
+        }
+    }
+
+    public event Action OnBegin;
 
     public void Begin()
     {
@@ -37,16 +75,21 @@ public abstract class BossFightManager : MonoBehaviour
     private IEnumerator BeginCo()
     {
         yield return PrepareCo();
-        OnBeginInterval();
-        DuringFight = true;
-        OnBegin?.Invoke();
-       
-    }
 
-    private void OnTriggerEnter(Collider triggerCollider)
-    {
-        IsTriggered = true;
-        HandleTriggerEnter();
+        CinematicPlayer.PlaySequence(beginCutscene, beginCutsceneTransform.position, beginCutsceneTransform.rotation,
+            beginCutsceneTransform.localScale);
+
+        CinematicPlayer.onEndPlay += OnCutsceneEnd;
+
+        yield break;
+
+        void OnCutsceneEnd()
+        {
+            OnBeginInterval();
+            duringFight = true;
+            OnBegin?.Invoke();
+            CinematicPlayer.onEndPlay -= OnCutsceneEnd;
+        }
     }
 
     protected abstract void HandleTriggerEnter();
@@ -54,6 +97,8 @@ public abstract class BossFightManager : MonoBehaviour
     protected abstract IEnumerator PrepareCo();
 
     protected abstract void OnBeginInterval();
+
+    protected abstract void HandleBossDeath();
 
     protected abstract void OnRestartInterval();
 }
