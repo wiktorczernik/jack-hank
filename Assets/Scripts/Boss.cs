47a4b3d23ccfb75d815260d0a85fd7383031b915
Dan <danik.serduk@gmail.com>
2025-04-22 19:43:21 +0200

    feat: add proto version of abstract class of game boss

---
+++ Assets/Scripts/Boss.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+public abstract class Boss : MonoBehaviour
+{
+    public event Action onDeath;
+
+    private void DieHandle()
+    {
+        PrepareDie();
+        onDeath?.Invoke();
+    }
+
+    protected abstract void PrepareDie();
+}
