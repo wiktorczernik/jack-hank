aa8d8871f237da84185edce5d1e9e0060799ef11
Dan <danik.serduk@gmail.com>
2025-04-22 14:13:00 +0200

    chore: add abstract manager for boss fight

---
+++ Assets/Scripts/BossFightManager.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public abstract class BossFightManager : MonoBehaviour
+{
+    public event Action OnBegin;
+    public bool DuringFight { get; private set; }
+    public bool IsTriggered { get; private set; }
+    
+    [SerializeField] protected GameObject bossPrefab;
+    [SerializeField] protected Transform bossSpawnPoint;
+    [SerializeField] private TriggerEventEmitter bossFightTrigger;
+
+    private void Awake()
+    {
+        if (!bossFightTrigger) 
+            Debug.LogError($"[{GetType().Name}] component in [{name}] game object: No trigger for boss fight!");
+        if (!bossPrefab) 
+            Debug.LogError($"[{GetType().Name}] component in [{name}] game object: No boss prefab!");
+        if (!bossSpawnPoint)
+            Debug.LogError($"[{GetType().Name}] component in [{name}] game object: No boss spawn point!");
+        
+        bossFightTrigger.OnEnter.AddListener((triggerCollider) => { IsTriggered = true; });
+    }
+
+    public void Begin()
+    {
+        StartCoroutine(BeginCo());
+    }
+
+    public void Restart()
+    {
+        OnRestartInterval();
+    }
+
+    private IEnumerator BeginCo()
+    {
+        yield return PrepareCo();
+        OnBeginInterval();
+        DuringFight = true;
+        OnBegin?.Invoke();
+       
+    }
+
+    protected abstract IEnumerator PrepareCo();
+
+    protected abstract void OnBeginInterval();
+
+    protected abstract void OnRestartInterval();
+}
