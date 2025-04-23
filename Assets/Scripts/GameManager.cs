c19eeb69a72e8e6ebfffbf65e9979b585320ef0d
Dan <danik.serduk@gmail.com>
2025-04-23 17:22:38 +0200

    feat: add level restart

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,19 +1,12 @@
 using System;
-using System.Linq;
 using LevelManagement;
 using LevelTask;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] private Bonus_GUI bonusGUI;
-    [SerializeField] private LevelDefinition debugLevelDefinition;
-    
     public static GameManager Local;
-    public static LevelTaskDefinition[] LevelTasks => _definition.LevelTasks; 
-    public static GameRunInfo RunInfo { get; private set; }
-    public static bool IsDuringRun { get; private set; }
-    public static PlayerVehicle PlayerVehicle { get; private set; }
 
     public static Action<GameRunInfo> OnRunBegin;
     public static Action<GameRunInfo> OnRunFinish;
@@ -21,6 +14,28 @@ public class GameManager : MonoBehaviour
     private static LevelDefinition _definition;
     private static LevelTaskTracker[] _levelTaskTrackers;
     private static bool _isInitialized;
+    [SerializeField] private Bonus_GUI bonusGUI;
+    [SerializeField] private LevelDefinition debugLevelDefinition;
+    [SerializeField] private FadeTransition_GUI fadeTransition;
+    public static LevelTaskDefinition[] LevelTasks => _definition.LevelTasks;
+    public static GameRunInfo RunInfo { get; private set; }
+    public static bool IsDuringRun { get; private set; }
+    public static PlayerVehicle PlayerVehicle { get; private set; }
+
+    private void Awake()
+    {
+        if (Debug.isDebugBuild && !_isInitialized)
+            Initialize(debugLevelDefinition);
+
+        if (!_isInitialized) Debug.LogError("Game Manager was not initialzied");
+
+        BeginRun();
+    }
+
+    private void Update()
+    {
+        if (IsDuringRun) GameRunFrameTick();
+    }
 
     public void Initialize(LevelDefinition definition)
     {
@@ -30,30 +45,30 @@ public class GameManager : MonoBehaviour
             Debug.LogError("Game Manager can not be initialized with null Level Definition");
             return;
         }
-        
+
         _definition = definition;
         CreateTaskTrackers();
         _isInitialized = true;
     }
-    
+
     public static void FinishRun()
     {
         IsDuringRun = false;
         OnRunFinish?.Invoke(RunInfo);
     }
-    
+
     public static void UpdateBonus(int bonusValue, PlayerBonusTypes bonusType, int bonusPool)
     {
         RunInfo.ChangeBonusBountyBy(bonusValue, bonusType);
         Local.bonusGUI.ShowBonus(bonusPool, bonusType);
     }
-    
-    public static void UpdateBonus(int bonusValue,  PlayerBonusTypes bonusType)
+
+    public static void UpdateBonus(int bonusValue, PlayerBonusTypes bonusType)
     {
         RunInfo.ChangeBonusBountyBy(bonusValue, bonusType);
         Local.bonusGUI.ShowBonus(bonusValue, bonusType);
     }
-    
+
     public static void UpdateDestructionCombo(int bonusValue, int combo, int bonusPool)
     {
         RunInfo.ChangeBonusBountyBy(bonusValue, PlayerBonusTypes.DestructionCombo);
@@ -70,7 +85,25 @@ public class GameManager : MonoBehaviour
 
         return LevelCompletenessMark.S;
     }
-    
+
+    public static void RestartLevel()
+    {
+        Local.fadeTransition.StartFadeIn();
+        Local.fadeTransition.OnFadeInEnded += AfterFadeIn;
+
+        return;
+
+        void AfterFadeIn()
+        {
+            SceneManager.LoadScene(_definition.SceneName);
+            Local.fadeTransition.OnFadeInEnded -= AfterFadeIn;
+        }
+    }
+
+    public static void RestartBossFight()
+    {
+    }
+
     private void BeginRun()
     {
         PlayerVehicle = FindFirstObjectByType<PlayerVehicle>();
@@ -83,35 +116,16 @@ public class GameManager : MonoBehaviour
     private void CreateTaskTrackers()
     {
         if (LevelTasks.Length == 0) return;
-        
+
         _levelTaskTrackers = new LevelTaskTracker[LevelTasks.Length];
         for (var i = 0; i < _levelTaskTrackers.Length; i++)
-        {
             _levelTaskTrackers[i] = LevelTaskTracker.CreateTracker(gameObject, LevelTasks[i]);
-        }
     }
-    
+
     private void GameRunFrameTick()
     {
         if (!IsDuringRun) return;
-        
-        RunInfo.Time += Time.deltaTime;   
-    }
-    
-    private void Awake()
-    {
-        if (Debug.isDebugBuild && !_isInitialized) 
-            Initialize(debugLevelDefinition);
 
-        if (!_isInitialized) Debug.LogError("Game Manager was not initialzied");
-        
-        BeginRun();
-    }
-    private void Update()
-    {
-        if (IsDuringRun)
-        {
-            GameRunFrameTick();
-        }
+        RunInfo.Time += Time.deltaTime;
     }
 }
