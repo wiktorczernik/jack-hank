a16bef1f22802f6650c47ba57f6a8f3fe20c24ad
Asystent <zielonyszlagrok@gmail.com>
2025-04-26 19:01:29 +0200

    feat: ammo crates and its effects
    come on ladies, come on ladies, one pound fish, come on ladies, come on ladies, one pound fish

---
+++ Assets/Scripts/PickupableAmmo.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PickupableAmmo : SmashableEntity
+{
+    [Header("Ammo Crate")]
+    public bool wasPickedUp = false;
+    public UnityEvent onPickedUp;
+
+    public int ammoInside;
+
+    public void NotifyPickup()
+    {
+        if (!isAlive || wasPickedUp)
+        {
+            return;
+        }
+        wasPickedUp = true;
+        hittable = false;
+        OnPickupEvent();
+        onPickedUp?.Invoke();
+    }
+    protected virtual void OnPickupEvent()
+    {
+        DisableColliders();
+        usedRigidbody.AddForce(Vector3.up * 5000, ForceMode.Force);
+    }
+    protected override void OnHitEvent()
+    {
+        base.OnHitEvent();
+        Kill();
+    }
+}
