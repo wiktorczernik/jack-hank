c6ff48154a2d0cbff02d04c6ecd322baace0b1c4
Wiktor Czernik <git@czernik.dev>
2025-04-26 19:02:12 +0200

    feat: Heliboss just tries to follow player!

--- Assets/Scripts/HeliBoss.cs
+++ Assets/Scripts/HeliBoss.cs
@@ -5,6 +5,13 @@ public class HeliBoss : BotVehicle
     [Header("Boss Properties")]
     public PlayerVehicle target;
     public Missle missilePrefab;
+    public GameObject ground;
+
+    [Header("Boss Follow Options")]
+    public float currentHeight = 0;
+    public float playerVerticalOffset = 20f;
+    public float playerForwardDistance = 30f;
+    public float verticalAlignSpeed = 1f;
 
     public void FuckingShoot()
     {
@@ -13,8 +20,40 @@ public class HeliBoss : BotVehicle
         instance.Shoot();
     }
 
-    protected void Start()
+    private void SeekPlayerView()
     {
-        InvokeRepeating(nameof(FuckingShoot), 2, 2);
+        arrived = false;
+        isFollowing = true;
+        destinationPoint = target.GetPosition();
+        destinationPoint.y = currentHeight;
+
+        Vector3 destinationOffset = Quaternion.Euler(Vector3.up * target.transform.eulerAngles.y) * Vector3.forward;
+        destinationOffset *= playerForwardDistance;
+
+        destinationPoint += destinationOffset;
+    }
+    private void AlignGround()
+    {
+        float targetHeight = target.transform.position.y + playerVerticalOffset;
+
+        currentHeight = Mathf.Lerp(currentHeight, targetHeight, verticalAlignSpeed * Time.fixedDeltaTime);
+
+        Vector3 newPos = transform.position;
+        newPos.y = currentHeight;
+
+        ground.transform.position = newPos;
+    }
+
+    protected override void Awake()
+    {
+        base.Awake();
+        currentHeight = transform.position.y;
+        //InvokeRepeating(nameof(FuckingShoot), 1f, 1f);
+    }
+    protected override void FixedUpdate()
+    {
+        base.FixedUpdate();
+        AlignGround();
+        SeekPlayerView();
     }
 }
