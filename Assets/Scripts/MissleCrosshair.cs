d04e5f40e0b33a4adfda5d531408f857ec0cb495
Viktor Chernikov <vktrchernikov@gmail.com>
2025-04-29 13:25:05 +0200

    rework: Heli burst firing reworked

---
+++ Assets/Scripts/MissleCrosshair.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class MissleCrosshair : MonoBehaviour
+{
+    public bool isVisible = false;
+
+    DecalProjector decal;
+
+    
+    public void SetPosition(Vector3 position)
+    {
+        transform.position = position;
+    }
+    public void Show()
+    {
+        if (isVisible)
+        {
+            Debug.LogError("Attemp of starting follow when its already following!", this);
+        }
+        isVisible = true;
+        decal.enabled = true;
+    }
+    public void Hide()
+    {
+        if (!isVisible)
+        {
+            Debug.LogError("Tried freeing crosshair that isn't following atm!", this);
+        }
+        isVisible = false;
+        decal.enabled = false;
+    }
+
+    private void Awake()
+    {
+        decal = GetComponentInChildren<DecalProjector>();
+        decal.enabled = false;
+    }
+}
