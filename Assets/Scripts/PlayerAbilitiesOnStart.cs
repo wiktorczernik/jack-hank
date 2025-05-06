72fbc2876d3e21ff83d7c1b6a7f7dffc51e338c9
Dan <danik.serduk@gmail.com>
2025-05-06 16:47:49 +0200

    feat: add trigger that switch player abilities

---
+++ Assets/Scripts/PlayerAbilitiesOnStart.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class PlayerAbilitiesOnLevelStart : MonoBehaviour
+{
+    [SerializeField] private PlayerVehicleAbilities playerAbilities;
+    [SerializeField] private bool hasNitro = true;
+    [SerializeField] private bool hasJump = true;
+
+    private void Start()
+    {
+        if (playerAbilities.jump)
+        {
+            if (hasJump) playerAbilities.jump.TurnOn();
+            else playerAbilities.jump.TurnOff();
+        }
+        
+        if (playerAbilities.nitro)
+        {
+            if (hasNitro) playerAbilities.nitro.TurnOn();
+            else playerAbilities.nitro.TurnOff();
+        }
+    }
+}
