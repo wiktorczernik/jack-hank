6c223839592ff692cd3d684a6ac62a082bf9dd3b
Asystent <zielonyszlagrok@gmail.com>
2025-05-07 20:21:45 +0200

    feat: Dialogue master

---
+++ Assets/DialogueMaster.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class DialogueMaster : MonoBehaviour
+{
+    public static DialogueMaster master;
+
+    public UnityEvent onDialogueStart;
+    public UnityEvent onDialogueEnd;
+    public List<AudioClip> dialogues;
+
+    private AudioSource _source;
+
+    public static bool isPlaying => master._source.isPlaying;
+    private bool oldIsPlaying = isPlaying;
+
+    private void Update()
+    {
+        if (isPlaying != oldIsPlaying)
+            if (isPlaying) onDialogueStart?.Invoke();
+            else onDialogueEnd?.Invoke();
+        oldIsPlaying = isPlaying;
+    }
+
+    private IEnumerator Start()
+    {
+        _source = GetComponent<AudioSource>();
+
+        yield return new WaitUntil(() => master == null);
+        master = this;
+    }
+
+    public static void ForceCallDialogue(int index)
+    {
+        master._source.clip = master.dialogues[index];
+        master._source.Play();
+    }
+    public static void ForceCallDialogue(string name)
+    {
+        master._source.clip = master.dialogues.Find(b => b.name == name);
+        master._source.Play();
+    }
+    public static void ForceCallDialogue(AudioClip clip)
+    {
+        master._source.clip = clip;
+        master._source.Play();
+    }
+}
