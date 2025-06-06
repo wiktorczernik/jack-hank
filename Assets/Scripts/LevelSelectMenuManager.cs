a28177751dd0fce869aeec92f643f5b464ee6e4c
Dan <danik.serduk@gmail.com>
2025-06-06 19:19:35 +0200

    chore: change levelSelection after last commits

--- Assets/Scripts/LevelSelectMenuManager.cs
+++ Assets/Scripts/LevelSelectMenuManager.cs
@@ -20,7 +20,7 @@ public class LevelSelectMenuManager : MonoBehaviour
     {
         if (State == LevelSelectMenuState.LevelSelected) return;
 
-        _instance._sceneExit.SetNextScene(selectedLevel.LevelSceneName);
+        _instance._sceneExit.SetNextLevel(selectedLevel.LevelID);
         
         _instance._levelScroller.StopScrolling();
         _instance._botVehicle.isFollowing = true;
