25d738a95388a931d26e85cec915624592a86ff6
Viktor Chernikov <vktrchernikov@gmail.com>
2025-06-06 18:34:05 +0200

    tweak: Main menu start button and bus stabilization

--- Assets/Scripts/GUI/PlayBtn_GUI.cs
+++ Assets/Scripts/GUI/PlayBtn_GUI.cs
@@ -1,14 +1,24 @@
+using System.Collections;
 using UnityEngine;
 
 public class PlayBtn_GUI : MonoBehaviour
 {
     bool activated = false;
-   
+    float startDelay = 3f;
+    bool waiting = false;
+
+    private IEnumerator Start()
+    {
+        yield return new WaitForSeconds(startDelay);
+        waiting = true;
+    }
+
     private void LateUpdate()
     {
-        if (Input.anyKeyDown && !activated)
+        if (Input.anyKeyDown && !activated && waiting)
         {
             activated = true;
+            ScreenFade.In(0.5f, ScreenFadeType.Circle);
             GameSceneManager.LoadLogin();
         }
     }
