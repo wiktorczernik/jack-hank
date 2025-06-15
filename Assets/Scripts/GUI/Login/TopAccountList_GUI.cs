9760b7675949dc4e9e0c6597baa570dee1b41d8c
Dan <danik.serduk@gmail.com>
2025-06-15 17:42:31 +0200

    feat: add leaders board

---
+++ Assets/Scripts/GUI/Login/TopAccountList_GUI.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AccountManagement;
+using TMPro;
+using UnityEngine;
+
+public class TopAccountList_GUI : MonoBehaviour
+{
+    [SerializeField] private RectTransform listItemPrefab;
+    [SerializeField] private TextMeshProUGUI  noAccountTextPrefab;
+    [SerializeField] private int amountOfTopAccountsToDisplay = 5;
+    [SerializeField] private float gapBetweenListItems = 5;
+    
+    private List<RectTransform> _listItems;
+    private TextMeshProUGUI _noAccountTextInstance;
+    
+    private void Start()
+    {
+        InitializeItemList();
+        
+        AccountManager.onCreateAccount += accountData => UpdateList();
+        AccountManager.onDeleteAccount += accountData => UpdateList();
+        
+        UpdateList();
+    }
+
+    private void UpdateList()
+    {
+        var accounts = AccountManager
+            .GetSavedAccounts()
+            .OrderByDescending(data => data.bountyPoints)
+            .Take(amountOfTopAccountsToDisplay)
+            .ToList();
+        
+        for (var i = 0; i < amountOfTopAccountsToDisplay; i++)
+        {
+            var textComponent = _listItems[i].gameObject.GetComponent<TextMeshProUGUI>();
+
+            textComponent.text = "";
+
+            if (i >= accounts.Count) continue;
+            
+            textComponent.text = $"{i + 1}. {accounts[i].accountName}";
+        }
+        
+        if (accounts.Count == 0)
+        {
+            Instantiate(noAccountTextPrefab, transform);
+        }else if (_noAccountTextInstance != null)
+        {
+            Destroy(_noAccountTextInstance);
+        }
+    }
+
+    private void InitializeItemList()
+    {
+        _listItems = new List<RectTransform>();
+
+        for (var i = 0; i < amountOfTopAccountsToDisplay; i++)
+        {
+            var item = Instantiate(listItemPrefab, transform);
+            item.anchoredPosition = new Vector2(0, -(i * item.rect.height + i * gapBetweenListItems));
+            _listItems.Add(item);
+        }
+    }
+}
