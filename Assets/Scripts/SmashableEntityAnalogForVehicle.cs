fbde6c2ed1c8a8a8064c12f8d80e91b8e838ebe2
Dan <danik.serduk@gmail.com>
2025-06-18 05:42:14 +0200

    feat: add debris for civilian car

--- Assets/Scripts/SmashableEntityAnalogForVehicle.cs
+++ Assets/Scripts/SmashableEntityAnalogForVehicle.cs
@@ -1,8 +1,12 @@
 ﻿using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.Serialization;
 
 public class SmashableEntityAnalogForVehicle : MonoBehaviour
 {
+    [SerializeField] private GameObject model;
+    [SerializeField] private GameObject debrisPrefab;
+    [SerializeField] private Rigidbody usedRigidbody;
     public int bountyPointsReward;
     public UnityEvent<SmashableEntityAnalogForVehicle> onHit;
 
@@ -15,7 +19,21 @@ public class SmashableEntityAnalogForVehicle : MonoBehaviour
 
     private void HandleOnEnter(Collision col)
     {
+        if (!col.gameObject.CompareTag("Player")) return;
+        
         if (_wasHit) return;
+        
+        usedRigidbody.freezeRotation = true;
+        usedRigidbody.constraints = RigidbodyConstraints.None;
+        usedRigidbody.isKinematic = true;
+        
+        if (debrisPrefab != null)
+        {
+            var modTrans = model.transform;
+            var debrisInst = Instantiate(debrisPrefab, modTrans.position, modTrans.rotation, transform);
+            debrisInst.transform.localScale = modTrans.localScale;
+            Destroy(model);
+        }
 
         onHit?.Invoke(this);
         
