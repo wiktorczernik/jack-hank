2be7733d76f18fe564711e9ce2b8586db5064160
Dan <danik.serduk@gmail.com>
2025-06-19 17:12:30 +0200

    feat: allow to wait CounterAnimationForNumberField_GUI.cs in other ways

--- Assets/Scripts/GUI/CounterAnimationForNumberField_GUI.cs
+++ Assets/Scripts/GUI/CounterAnimationForNumberField_GUI.cs
@@ -8,120 +8,149 @@ public class CounterAnimationForNumberField_GUI : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI usedTextComponent;
     [Min(0)][SerializeField] private float durationInSeconds = 1;
-    [Min(0)] [SerializeField] private int animationFPS = 60;
+    [Min(0)][SerializeField] private int animationFPS = 60;
 
     public bool isBusy { get; private set; }
+
+    public UnityEvent onAnimationStartedOnce;
     public UnityEvent onAnimationStarted;
+    public UnityEvent onAnimationEndedOnce;
     public UnityEvent onAnimationEnded;
 
-    public void StartNumericAnimation(double targetValue, double startValue = 0, bool useLeadingZeros = false, int digitsAfterPoint = 2)
+    public bool StartNumericAnimation(double targetValue, double startValue = 0, bool useLeadingZeros = false,
+        int digitsAfterPoint = 2)
     {
-        if (isBusy || targetValue - startValue == double.Epsilon) return;
-        isBusy = true;
-        
-        onAnimationStarted.Invoke();
-        StartCoroutine(AnimationCo());
-
-        IEnumerator AnimationCo()
-        {
-            var counter = startValue;
-            var step = (targetValue - startValue) / animationFPS;
-            var stepDuration = durationInSeconds / animationFPS;
-            
-            var format = useLeadingZeros ? 
-                new string('0', GetMaximumDigitsFrom(targetValue, startValue)) + "." + new string('0', digitsAfterPoint): 
-                "." + new string('0', digitsAfterPoint);
-
-            for (var i = 0; i < animationFPS * durationInSeconds; i++)
-            {
-                counter += step;
-                
-                if (targetValue - startValue < 0 && counter <= targetValue) break;
-                if (targetValue - startValue > 0 && counter >= targetValue) break;
-                
-                usedTextComponent.text = counter.ToString(format);
-                yield return new WaitForSeconds(stepDuration);
-            }
-
-            usedTextComponent.text = targetValue.ToString(format);
-            isBusy = false;
-            onAnimationEnded.Invoke();
-        }
+        StartCoroutine(StartNumericAnimationCo(targetValue, startValue, useLeadingZeros, digitsAfterPoint));
+        return isBusy;
     }
     
-    public void StartNumericAnimation(int targetValue, int startValue = 0, bool useLeadingZeros = false)
+    public IEnumerator StartNumericAnimationCo(double targetValue, double startValue = 0, bool useLeadingZeros = false,
+        int digitsAfterPoint = 2)
     {
-        if (isBusy || targetValue - startValue == 0) return;
+        if (isBusy || targetValue - startValue == double.Epsilon) yield break;
         isBusy = true;
-        
+
         onAnimationStarted.Invoke();
-        StartCoroutine(AnimationCo());
 
-        IEnumerator AnimationCo()
+        onAnimationStartedOnce.Invoke();
+        onAnimationStartedOnce.RemoveAllListeners();
+
+        var counter = startValue;
+        var step = (targetValue - startValue) / animationFPS;
+        var stepDuration = durationInSeconds / animationFPS;
+
+        var format = useLeadingZeros
+            ? new string('0', GetMaximumDigitsFrom(targetValue, startValue)) + "." + new string('0', digitsAfterPoint)
+            : "." + new string('0', digitsAfterPoint);
+
+        for (var i = 0; i < animationFPS * durationInSeconds; i++)
         {
-            var counter = startValue;
-            var stepsAmount = animationFPS * durationInSeconds;
-            var step = (int)((targetValue - startValue) / stepsAmount);
-            var format = useLeadingZeros ? new string('0', GetMaximumDigitsFrom(targetValue, startValue)) : "";
+            counter += step;
 
-            if (step == 0) step = 1;
-            
-            var stepDuration = durationInSeconds / animationFPS;
+            if (targetValue - startValue < 0 && counter <= targetValue) break;
+            if (targetValue - startValue > 0 && counter >= targetValue) break;
 
-            for (var i = 0; i < stepsAmount; i++)
-            {
-                counter += step;
-
-                if (targetValue - startValue < 0 && counter <= targetValue) break;
-                if (targetValue - startValue > 0 && counter >= targetValue) break;
-                
-                usedTextComponent.text = counter.ToString(format);
-                
-                yield return new WaitForSeconds(stepDuration);
-            }
-
-            usedTextComponent.text = targetValue.ToString(format);
-            isBusy = false;
-            onAnimationEnded.Invoke();
+            usedTextComponent.text = counter.ToString(format);
+            yield return new WaitForSeconds(stepDuration);
         }
+
+        usedTextComponent.text = targetValue.ToString(format);
+        isBusy = false;
+        onAnimationEnded.Invoke();
+
+        onAnimationEndedOnce.Invoke();
+        onAnimationEndedOnce.RemoveAllListeners();
+    }
+    
+    public bool StartNumericAnimation(int targetValue, int startValue = 0, bool useLeadingZeros = false)
+    {
+        StartCoroutine(StartNumericAnimationCo(targetValue, startValue, useLeadingZeros));
+        return isBusy;
     }
 
-    public void StartTimestampAnimation(int targetTimestamp, int startTimestamp = 0)
+    public IEnumerator StartNumericAnimationCo(int targetValue, int startValue = 0, bool useLeadingZeros = false)
     {
-        if (isBusy || targetTimestamp - startTimestamp == 0 || targetTimestamp < 0 || startTimestamp < 0) return;
+        if (isBusy || targetValue - startValue == 0) yield break;
         isBusy = true;
-        
+
         onAnimationStarted.Invoke();
-        StartCoroutine(AnimationCo());
 
-        IEnumerator AnimationCo()
+        onAnimationStartedOnce.Invoke();
+        onAnimationStartedOnce.RemoveAllListeners();
+        
+        var counter = startValue;
+        var stepsAmount = animationFPS * durationInSeconds;
+        var step = (int)((targetValue - startValue) / stepsAmount);
+        var format = useLeadingZeros ? new string('0', GetMaximumDigitsFrom(targetValue, startValue)) : "";
+
+        if (step == 0) step = 1;
+
+        var stepDuration = durationInSeconds / animationFPS;
+
+        for (var i = 0; i < stepsAmount; i++)
         {
-            var counter = startTimestamp;
-            var step = (targetTimestamp - startTimestamp) / animationFPS;
-            var stepDuration = durationInSeconds / animationFPS;
-            var format = "{0:00}:{1:00}";
+            counter += step;
 
-            for (var i = 0; i < animationFPS * durationInSeconds; i++)
-            {
-                counter += step;
-                
-                if (targetTimestamp - startTimestamp < 0 && counter <= targetTimestamp) break;
-                if (targetTimestamp - startTimestamp > 0 && counter >= targetTimestamp) break;
+            if (targetValue - startValue < 0 && counter <= targetValue) break;
+            if (targetValue - startValue > 0 && counter >= targetValue) break;
 
-                var seconds = (counter / 1000) % 60;
-                var minutes = (counter / 60000);
-                
-                usedTextComponent.text = String.Format(format, minutes, seconds);
-                yield return new WaitForSeconds(stepDuration);
-            }
+            usedTextComponent.text = counter.ToString(format);
 
-            var targetSeconds = (targetTimestamp / 1000) % 60;
-            var targetMinutes = (targetTimestamp / 60000);
-            
-            usedTextComponent.text = String.Format(format, targetMinutes, targetSeconds);
-            isBusy = false;
-            onAnimationEnded.Invoke();
+            yield return new WaitForSeconds(stepDuration);
         }
+
+        usedTextComponent.text = targetValue.ToString(format);
+        isBusy = false;
+        onAnimationEnded.Invoke();
+
+        onAnimationEndedOnce.Invoke();
+        onAnimationEndedOnce.RemoveAllListeners();
+    }
+
+    public bool StartTimestampAnimation(int targetTimestamp, int startTimestamp = 0)
+    {
+        StartCoroutine(StartTimestampAnimationCo(targetTimestamp, startTimestamp));
+        return isBusy;
+    }
+
+    public IEnumerator StartTimestampAnimationCo(int targetTimestamp, int startTimestamp = 0)
+    {
+        if (isBusy || targetTimestamp - startTimestamp == 0 || targetTimestamp < 0 || startTimestamp < 0) yield break;
+        isBusy = true;
+
+        onAnimationStarted.Invoke();
+
+        onAnimationStartedOnce.Invoke();
+        onAnimationStartedOnce.RemoveAllListeners();
+        
+        var counter = startTimestamp;
+        var step = (targetTimestamp - startTimestamp) / animationFPS;
+        var stepDuration = durationInSeconds / animationFPS;
+        var format = "{0:00}:{1:00}";
+
+        for (var i = 0; i < animationFPS * durationInSeconds; i++)
+        {
+            counter += step;
+
+            if (targetTimestamp - startTimestamp < 0 && counter <= targetTimestamp) break;
+            if (targetTimestamp - startTimestamp > 0 && counter >= targetTimestamp) break;
+
+            var seconds = (counter / 1000) % 60;
+            var minutes = (counter / 60000);
+
+            usedTextComponent.text = String.Format(format, minutes, seconds);
+            yield return new WaitForSeconds(stepDuration);
+        }
+
+        var targetSeconds = (targetTimestamp / 1000) % 60;
+        var targetMinutes = (targetTimestamp / 60000);
+
+        usedTextComponent.text = String.Format(format, targetMinutes, targetSeconds);
+        isBusy = false;
+        onAnimationEnded.Invoke();
+
+        onAnimationEndedOnce.Invoke();
+        onAnimationEndedOnce.RemoveAllListeners();
     }
 
     private int GetMaximumDigitsFrom(double a, double b)
