e376eb7454dc318f8e7dc8c9cae8beb7b302ac7a
Wiktor Czernik <git@czernik.dev>
2025-06-26 05:43:42 +0200

    chore: Fixed passenger UI

--- Assets/Scripts/GUI/PassengerIInteraction_GUI.cs
+++ Assets/Scripts/GUI/PassengerIInteraction_GUI.cs
@@ -5,41 +5,29 @@ public class PassengerInteraction_GUI : MonoBehaviour
 {
     static PassengerInteraction_GUI inst;
 
-    [SerializeField] GameObject Catch;
-    [SerializeField] GameObject Smash;
-    [SerializeField] GameObject Expire;
+    [SerializeField] Animation Catch;
+    [SerializeField] Animation Smash;
+    [SerializeField] Animation Expire;
 
     public float caughtUITime = 3.5f;
     public float smashedUITime = 3.5f;
     public float expiredUITime = 3.5f;
 
-    private void Awake()
+    private void OnEnable()
     {
-        if (inst != null) Destroy(this);
-        else inst = this;
+        inst = this;
     }
 
     public static void PassengerCaught()
     {
-        inst.StartCoroutine(inst.ShowForTime(inst.Catch, inst.caughtUITime));
+        inst.Catch.Play();
     }
     public static void PassengerSmashed()
     {
-        inst.StartCoroutine(inst.ShowForTime(inst.Smash, inst.smashedUITime));
+        inst.Smash.Play();
     }
     public static void PassengerExpired()
     {
-        inst.StartCoroutine(inst.ShowForTime(inst.Expire, inst.expiredUITime));
-    }
-
-    IEnumerator ShowForTime(GameObject obj, float time)
-    {
-        obj.SetActive(true);
-        while (time >= 0f)
-        {
-            yield return new WaitForEndOfFrame();
-            time -= Time.deltaTime;
-        }
-        obj.SetActive(false);
+        inst.Expire.Play();
     }
 }
